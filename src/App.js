a294f99dfce7c6d619e1699ac9dc4e39b35f85f1
Greg <gregcourt10@gmail.com>
2023-03-27 12:05:09 +0100

    dataloading

--- src/App.js
+++ src/App.js
@@ -11,12 +11,20 @@ import Projects from "./Projects";
 import Contact from "./Contact";
 
 function App() {
+  const loadCountries = async () => {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = await response.json();
+    return data;
+  };
+
+  
+
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Welcome />,
       children: [
-        { path: "/about", element: <About /> },
+        { path: "/about", element: <About />, loader: loadCountries },
         { path: "/projects", element: <Projects /> },
         { path: "/contact", element: <Contact /> },
       ],
